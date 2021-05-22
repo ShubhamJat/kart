758aad67c9d8e40d3b7caababf06b45713beb8f2
Shubham <75352915+ShubhamJat@users.noreply.github.com>
2021-05-22 14:50:22 +0530

    launch

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React, { Component } from 'react';
+import './App.css';
+import Shop from "./components/Shop";
+
+class App extends Component {
+  
+  render() {
+    const items = [
+      {
+        heading :"Cap - $5",
+        name :"Cap",
+        price:5
+      },
+      {
+        heading :"Hand Bag - $30",
+        name :"HandBag",
+        price:30
+      },
+      {
+        heading :"Shirt - $35",
+        name :"Shirt",
+        price:35
+      },
+      {
+        heading :"Shoes - $50",
+        name :"Shoe",
+        price:50
+      },
+      {
+        heading :"Pant - $35",
+        name :"Pant",
+        price:35
+      },
+      {
+        heading :"Slipper - $25",
+        name :"Slipper",
+        price:25
+      },
+    ]
+
+    return (
+      <div className="App">
+        <div >
+         <img style={{float:'left'}} alt="Your Cart" width="75"  height="75" src="/sj.png" className="d-inline-block align-top logo" />
+         <h1 style={{ paddingRight: "1163px" , width:'264px'}} >
+          Jatt's Shop
+        </h1>
+        </div>
+        <Shop items={items}/>
+      </div>
+    );
+  }
 }
 
 export default App;
+
+
---
+++ src/components/Shop.js
@@ -0,0 +1,98 @@
+
+import React, { useState } from "react";
+import Grid from '@material-ui/core/Grid';
+import Divider from '@material-ui/core/Divider';
+import RightPanel from './rightPanel';
+import LeftPanel from './leftPanel';
+
+ const Shop = (props) => {
+    const { items } = props
+    const [cart, setCart] = useState([]);
+    //console.log(cart);
+  //  console.log(items);
+    const [subTotal, setSubTotal] = useState(0);
+    const [coupon, setCoupon] = useState(0);
+
+
+    const addItem = (item) => {
+       const check= (temp) => {
+           if(temp.name === a.name){
+               temp.quantity = temp.quantity +1;
+               return true;
+           }
+           else return false;
+       }
+        var a= {name : item.name, quantity : 1, totalPrice : item.price };
+        if(cart.some(check)){
+             var filtered = cart.filter(function(value, index, arr){ 
+        return value.quantity > 0;
+    });
+    setCart(filtered);
+   // console.log(cart);
+        }
+       else {
+//console.log("else");
+            var x= cart;
+            x.push(a);
+           //cart.push(a);
+            setCart(x);
+       }
+     
+    }
+
+    const removeItem = (item) => {
+         const check= (temp) => {
+           if(temp.name === a.name){
+               temp.quantity = temp.quantity - 1;
+               return true;
+           }
+           else return false;
+       }
+        var a= {name : item.name, quantity : 1, totalPrice : item.price };
+        if(cart.some(check)){
+             var filtered = cart.filter(function(value, index, arr){ 
+        return value.quantity > 0;
+    });
+    setCart(filtered);
+   // console.log(cart);
+        }
+       else{
+           
+       }
+      
+    }
+
+    const calcSubTotal = (newCart, tempCoupon) => {
+
+        if (newCart !== undefined && newCart !== null && newCart !== []) {
+            setSubTotal(0);
+            console.log(newCart);
+            console.log(tempCoupon);
+            var sumt =0;
+            for(var i=0;i<newCart.length;i++){
+                sumt=sumt + parseInt(newCart[i].totalPrice) * parseInt(newCart[i].quantity);
+            }
+            sumt = sumt * ((100 - tempCoupon)/100);
+            setSubTotal(sumt);
+            
+        }
+    }
+
+    const handleCouponChange = event => {
+        setCoupon(event.target.value);
+        calcSubTotal(cart, event.target.value);
+    };
+
+    return (
+        <div>
+            <Grid className='GridRoot'>
+                <LeftPanel items={items} addItem={addItem} removeItem={removeItem}/>
+                <Divider orientation="vertical" />
+                <RightPanel cart={cart} coupon={coupon}
+                    handleCouponChange={handleCouponChange} subTotal={subTotal} />
+            </Grid>
+        </div>
+    );
+}
+
+export default Shop;
